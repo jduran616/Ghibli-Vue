8fa06186ac510acbe4e16d64d7e5d8d9e0e924d7
JD <jduran005@student.butte.edu>
2019-04-04 15:35:28 -0700

    Changed Card Component
    Revoved Movies component and integrated straight into Card component

--- Ghibli-vue/src/App.vue
+++ Ghibli-vue/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
+    <Logo />
     <div class="container">
-      <Logo />
-      <Card />
+      <Card v-bind:movies="movies" />
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Logo from './components/Logo';
 import Card from './components/Card';
-import axios from 'axios';
 
 export default {
   name: 'app',
@@ -25,9 +25,8 @@ export default {
   },
   methods: {
     created() {
-      axios.get('https://ghibliapi.herokuapp.com/films')
-      // .then(res => this.movies = res.data)
-      .then(res => console.log(res.data))
+      axios.get('https://ghibliapi.herokuapp.com/films/')
+      .then(res => this.movies = res.data)
       .catch(err => console.log(err));
     }
   }
--- Ghibli-vue/src/components/Card.vue
+++ Ghibli-vue/src/components/Card.vue
@@ -1,18 +1,15 @@
 <template>
     <div>
         <div class="card" v-bind:key="movie.id" v-for="movie in movies">
-            <Movies v-bind:movie="movie" />
+            <h1>{{movie.title}}</h1>
+            <p>{{movie.url}}</p>
         </div>
     </div>
 </template>
 
 <script>
-import Movies from './Movies'
 export default {
     name: "Card",
-    components: {
-        Movies
-    },
     props: ["movies"]
 }
 </script>
@@ -42,5 +39,18 @@ export default {
         .card {
             flex: 1 1 calc(33% - 2rem);
         }
+    }
+
+    h1 {
+        text-align: center;
+        margin: 0 0 2rem 0;
+        padding: 1rem 2rem;
+        font-size: 1.5rem;
+    }
+
+    p {
+        line-height: 1.5rem;
+        padding: 0 1.5rem 1.5rem;
+        color: rgb(77, 20, 62);
     }
 </style>
